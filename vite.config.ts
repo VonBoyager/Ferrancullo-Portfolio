a33c0bef7a03fc7eee1bbd604db15cb6a82fe7b7
Ced <ced@example.com>
2025-10-10 18:08:49 +0800

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ferrancullo-portfolio/', // Replace 'ferrancullo-portfolio' with your GitHub repository name
+  base: '/Ferrancullo-Portfolio/', // Replace 'Ferrancullo-Portfolio' with your GitHub repository name
   build: {
     outDir: 'dist',
     sourcemap: false,
